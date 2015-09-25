a0909669f84bde23ed8a9965d8d1349a57188e09
Katie Byers <lobsterkatie@gmail.com>
2015-09-25 06:44:24 -0700

    working

--- accounting2.py
+++ accounting2.py
@@ -1,6 +1,47 @@
 def main():
     MELON_COST = 1.00
 
+    transaction_list = get_customer_data_from_log()
+
+    for sale in transaction_list:
+        name = sale[0]
+        num_melons = sale[1]
+        payment = sale[2]
+        check_customer_payment(name, num_melons, MELON_COST, payment)
+
+
+def get_customer_data_from_log(log_file_name="customer-orders.txt"):
+    """ Creates and returns a list containing customer data, each customer
+        being represented as a list itself.
+
+        Expects log file to be in format
+            t_id|name|num|payment
+        where
+            t_id is a transaction ID
+            name is the customer's name
+            num is the number of melons purchased
+            payment is the amount the customer actually paid   .
+
+        All data save transaction ID are passed along as a list in the same
+        order as above.
+    """
+
+    log_file = open(log_file_name)
+
+    transaction_list = []
+
+    for line in log_file:
+        data = line.split("|")
+        data[2] = int(data[2]) #number of melons
+        data[3] = float(data[3]) #actual payment
+        del data[0] #don't need transacton ID
+        transaction_list.append(data) #add new transaction to the list
+
+    return transaction_list
+
+
+
+
 def check_customer_payment(name, num_melons, melon_cost, payment):
     """ Computes expected payment and compares it to actual payment. Prints a
         message to the screen if actual != expected. Returns nothing. """
@@ -11,59 +52,8 @@ def check_customer_payment(name, num_melons, melon_cost, payment):
         print out_str.format(name = name, actual = payment, expected = expected_payment)
 
 
-customer1_name = "Joe"
-customer1_melons = 5
-customer1_paid = 5.00
 
-customer2_name = "Frank"
-customer2_melons = 6
-customer2_paid = 6.00
 
-customer3_name = "Sally"
-customer3_melons = 3
-customer3_paid = 3.00
-
-customer4_name = "Sean"
-customer4_melons = 9
-customer4_paid = 9.50
-
-customer5_name = "David"
-customer5_melons = 4
-customer5_paid = 4.00
-
-customer6_name = "Ashley"
-customer6_melons = 3
-customer6_paid = 2.00
-
-customer1_expected = customer1_melons * melon_cost
-if customer1_expected != customer1_paid:
-    print customer1_name, "paid {:.2f}, expected {:.2f}".format(
-        customer1_paid, customer1_expected)
-
-customer2_expected = customer2_melons * melon_cost
-if customer2_expected != customer2_paid:
-    print customer2_name, "paid {:.2f}, expected {:.2f}".format(
-        customer2_paid, customer2_expected)
-
-customer3_expected = customer3_melons * melon_cost
-if customer3_expected != customer3_paid:
-    print customer3_name, "paid {:.2f}, expected {:.2f}".format(
-        customer3_paid, customer3_expected)
-
-customer4_expected = customer4_melons * melon_cost
-if customer4_expected != customer4_paid:
-    print customer4_name, "paid {:.2f}, expected {:.2f}".format(
-        customer4_paid, customer4_expected)
-
-customer5_expected = customer5_melons * melon_cost
-if customer5_expected != customer5_paid:
-    print customer5_name, "paid {:.2f}, expected {:.2f}".format(
-        customer5_paid, customer5_expected)
-
-customer6_expected = customer6_melons * melon_cost
-if customer6_expected != customer6_paid:
-    print customer6_name, "paid {:.2f}, expected {:.2f}".format(
-        customer6_paid, customer6_expected)
 
 
 main()
